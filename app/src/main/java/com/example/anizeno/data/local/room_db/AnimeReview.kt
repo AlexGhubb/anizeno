01b65a5bc4d7b9090b91777fe05d74093480bb08
AlexGhubb <asortor2504@g.educaand.es>
2025-11-27 17:29:49 +0100

    test SQLite

--- app/src/main/java/com/example/anizeno/data/local/room_db/AnimeReview.kt
+++ app/src/main/java/com/example/anizeno/data/local/room_db/AnimeReview.kt
@@ -1,9 +1,25 @@
 package com.example.anizeno.data.local.room_db
 
 import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.Index
 import androidx.room.PrimaryKey
 
-@Entity("anime_reviews")
+@Entity("anime_reviews",
+//    foreignKeys = [
+//        ForeignKey(
+//            entity = UserProfile::class,
+//            parentColumns = ["email"],
+//            childColumns = ["userEmail"],
+//            onDelete = ForeignKey.CASCADE
+//        )
+//    ],
+//    indices = [
+//        Index("malId"),
+//        Index("userEmail"),
+//        Index(value = ["malId", "userEmail"], unique = true)
+//    ]
+)
 data class AnimeReview(
     @PrimaryKey(autoGenerate = true) val id:Int = 0,
     val malId:Int,
--- app/src/main/java/com/example/anizeno/data/local/room_db/UserAnimeData.kt
+++ app/src/main/java/com/example/anizeno/data/local/room_db/UserAnimeData.kt
@@ -3,11 +3,28 @@ package com.example.anizeno.data.local.room_db
 
 import android.os.Parcelable
 import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.Index
 import androidx.room.PrimaryKey
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
-@Entity(tableName = "user_anime_list")
+@Entity(
+    tableName = "user_anime_list",
+//    foreignKeys = [
+//        ForeignKey(
+//            entity = UserProfile::class,
+//            parentColumns = ["email"],
+//            childColumns = ["userEmail"],
+//            onDelete = ForeignKey.CASCADE
+//        )
+//    ],
+//    indices = [
+//        Index("userEmail"),
+//        Index("malId"),
+//        Index(value = ["userEmail", "malId", "category"], unique = true)
+//    ]
+)
 data class UserAnimeData(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
     val malId: Int?,
